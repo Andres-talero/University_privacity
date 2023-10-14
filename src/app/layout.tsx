9edbbbe3a313bb0acec6410b9545076c70f5150d
Andres_Talero04 <andresyjp2204@gmail.com>
2023-10-13 20:00:31 -0500

    NextUi configure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { NextUIProvider } from "@nextui-org/react";
 
 export const metadata: Metadata = {
   title: "University EAN - Privacity",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <NextUIProvider>
+        <body>{children}</body>
+      </NextUIProvider>
     </html>
   );
 }
